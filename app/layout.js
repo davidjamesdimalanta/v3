546cecf43020eaa4a4b16e0a12c1017fd9655c1a
David Dimalanta <david.dimalanta@mail.utoronto.ca>
2026-02-02 13:59:20 -0500

    analytics

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import SmoothScroll from "./ui/SmoothScroll";
 import WaveBackground from "./ui/WaveBackground";
 import { inter, newRodinPro, aspekta } from "./fonts";
 import FontLoadingMonitor from "./components/FontLoadingMonitor";
+import { Analytics } from "@vercel/analytics/next"
 
 
 export const metadata = {
@@ -58,6 +59,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${inter.variable} ${newRodinPro.variable} ${aspekta.variable}`}>
       <body className="relative antialiased">
+        <Analytics/>
         <FontLoadingMonitor />
         <SmoothScroll />
         <WaveBackground />
